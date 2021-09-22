f261440c8c69b28b8d3c2f4d91a9b3431d89a957
EC2 Default User <ec2-user@ip-172-31-81-106.ec2.internal>
2021-09-22 17:07:37 +0000

    Fix keypair

--- terraform/ecs/variables.tf
+++ terraform/ecs/variables.tf
@@ -11,7 +11,7 @@ variable "availability_zones" {
 
 variable "key_pair" {
   description = "AWS EC2 Key Pair Pem"
-  default     = "devrel-angel-rivera"
+  default     = "emueller-aws"
 }
 
 variable "ami" {
